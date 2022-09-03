3c0b27ca0c65ca8dfb4bd07a3ae66fa22ac9455e
Udaykiran3543 <112744722+Udaykiran3543@users.noreply.github.com>
2022-09-03 15:18:47 +0530

    Update and rename HelloWorld.java to calculator.java

---
+++ calculator.java
@@ -0,0 +1,13 @@
+public class calculator {
+  public static void main(String[] args) {
+    int a = 50,b=10;
+
+    System.out.println("addition of two num : " + (a+b));
+    
+     System.out.println("subtarction of two num : " + (a-b));
+    
+     System.out.println("multiplication of two num : " + (a*b));
+    
+     System.out.println("division of two num : " + (a/b));
+  }
+}
